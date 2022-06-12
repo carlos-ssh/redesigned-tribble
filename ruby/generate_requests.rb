023f75929fef4c958a06283440948026c07a8c3f
CARLOS-SSH <carlos.anezeh@gmail.com>
2022-06-11 20:40:09 -0500

    Fix merge issues.

--- ruby/generate_requests.rb
+++
@@ -1,9 +0,0 @@
-require 'json'
-require 'rest-client'
-
-url = 'https://api.currencyapi.com/v3/latest?apikey=4ijRkSxGPdVuYxlIsQ3zwDnIUojKUAazysyJkeEI&currencies='
-
-response = RestClient.get url
-result = JSON.parse response.to_str
-
-puts result
